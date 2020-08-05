55e56f688ac5732be683dbde319dca2e52657a26
Malvika Kumar <malvikavijaykumar@gmail.com>
2020-08-05 16:24:42 -0500

    CharReplace

---
+++ CharReplace.java
@@ -0,0 +1,11 @@
+public class CharReplace {
+    public static void main(String[] args) {
+        String str = "Mr John Smith   ";
+
+        str = str.trim();
+        System.out.println(str);
+
+        str = str.replaceAll(" ", "%20");
+        System.out.println(str);        
+    }
+}
