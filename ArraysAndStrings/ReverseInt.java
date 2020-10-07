1d3794553308e37dd2e2e1c1014d82e90419323c
malvikakumar <malvikavijaykumar@gmail.com>
2020-10-07 12:04:33 -0500

    ReverseInt

---
+++ ArraysAndStrings/ReverseInt.java
@@ -0,0 +1,12 @@
+public class ReverseInt {
+    public static void main(String[] args) {
+        int num = 123;
+        int result = 0;
+        while(num > 0) {
+            int r = num%10;
+            result = result * 10 + r;
+            num /= 10;
+        }
+        System.out.println(result);
+    }
+}
